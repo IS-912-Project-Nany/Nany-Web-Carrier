fcf121de50d872a4279fe6b80f5eaded814d9733
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-06-17 01:02:56 -0600

    [NEW] Requisitos component

---
+++ src/app/requisitos/requisitos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requisitos',
+  templateUrl: './requisitos.component.html'
+})
+export class RequisitosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
