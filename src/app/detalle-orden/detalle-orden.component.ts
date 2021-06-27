902e8125900fde8e551e2a9fbff4a2064e2f042e
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-06-27 16:20:42 -0600

    [ADD] Order details component

---
+++ src/app/detalle-orden/detalle-orden.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-orden',
+  templateUrl: './detalle-orden.component.html'
+})
+export class DetalleOrdenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
