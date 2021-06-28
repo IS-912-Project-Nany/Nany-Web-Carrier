b798db58b29c5d4817666af8f1bd33f79da2b1fb
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-06-27 18:07:02 -0600

    [ADD] Historial order component

---
+++ src/app/historial-ordenes/historial-ordenes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-ordenes',
+  templateUrl: './historial-ordenes.component.html'
+})
+export class HistorialOrdenesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
