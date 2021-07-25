12197215abb9cd5c031dbfeaba5a351561fdb6c7
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-07-24 18:46:25 -0600

    [ADD] Temporal JSON in historial-ordenes

--- src/app/historial-ordenes/historial-ordenes.component.ts
+++ src/app/historial-ordenes/historial-ordenes.component.ts
@@ -5,6 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './historial-ordenes.component.html'
 })
 export class HistorialOrdenesComponent implements OnInit {
+  historialOrdenes:any= [
+    {
+      destino:'Col. La Travesia',
+      nombreComprador:'Melany Avila',
+      totalFactura:'L. 990.00',
+      comision:'Lps. 100',
+      fecha: '08/09/2021'
+    },
+    {
+      destino:'Col. San Miguel',
+      nombreComprador:'Oscar Avila',
+      totalFactura:'L. 990.00',
+      comision:'Lps. 100',
+      fecha: '05/07/2021'
+    },
+    {
+      destino:'Villanueva',
+      nombreComprador:'Carla Medina',
+      totalFactura:'L. 990.00',
+      comision:'Lps. 100',
+      fecha: '22/06/2021'
+    },
+    {
+      destino:'Col. Tiloarque',
+      nombreComprador:'Carmen Medina',
+      totalFactura:'L. 990.00',
+      comision:'Lps. 90',
+      fecha: '22/06/2021'
+    },
+  ];
 
   constructor() { }
 
