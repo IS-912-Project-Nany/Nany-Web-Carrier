3d362b42ebc19a2013c5e9ea29b8a87f88ca22fd
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-07-24 18:45:50 -0600

    [ADD] Temporal JSON in homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -5,6 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './homepage.component.html'
 })
 export class HomepageComponent implements OnInit {
+  ordenesDisponibles:any = [
+    {
+      destino:'Col. La Travesia',
+      nombreComprador:'Marcos Torres',
+      cantidad:'1 unidad',
+      horaCompra:'05:40 pm',
+      imgOrden:'zapatillas.png'
+    },
+    {
+      destino:'Col. San Miguel',
+      nombreComprador:'Martha Molina',
+      cantidad:'1 unidad',
+      horaCompra:'05:40 pm',
+      imgOrden:'vestir.png'
+    }
+  ];
+
+  ordenesTomadas:any = [
+    {
+      destino:'Col. Miraflores',
+      nombreComprador:'Camilo Acosta',
+      cantidad:'1 unidad',
+      horaCompra:'04:30 pm',
+      imgOrden:'juguetes.png'
+    },
+    {
+      destino:'Col. Tiloarque',
+      nombreComprador:'Molly Cruz',
+      cantidad:'1 unidad',
+      horaCompra:'04:30 pm',
+      imgOrden:'juguetes.png'
+    },
+    
+  ]
 
   constructor() { }
 
