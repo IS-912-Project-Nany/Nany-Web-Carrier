d9c7ac3dbda82ef55b327f381fff8a6c9aa20cc5
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-07-24 18:46:44 -0600

    [ADD] Temporal JSON in detalle-orden

--- src/app/detalle-orden/detalle-orden.component.ts
+++ src/app/detalle-orden/detalle-orden.component.ts
@@ -5,6 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './detalle-orden.component.html'
 })
 export class DetalleOrdenComponent implements OnInit {
+  detalleOrden:any = [
+    {
+      nombreArticulo:'Tenis de colores',
+      descripcion:'Zapato marca Nike, color: Rojo, numero EUR38, USA 8',
+      cantidad:'1 unidad',
+      precio:'Lps. 1,000',
+      categoria:"Zapatos",
+      empresa:'Payless',
+      imgOrden:'zapatillas.png'
+    }
+  ]
 
   constructor() { }
 
