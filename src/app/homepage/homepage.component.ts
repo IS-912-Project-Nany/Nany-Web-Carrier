29c6e79e99b4023a144959038d56208a491d3cbf
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-08-18 01:27:13 -0600

    [FIX] Services homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,48 +1,120 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { OrdenesService } from '../services/ordenes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html'
 })
 export class HomepageComponent implements OnInit {
-  ordenesDisponibles:any = [
-    {
-      destino:'Col. La Travesia',
-      nombreComprador:'Marcos Torres',
-      cantidad:'1 unidad',
-      horaCompra:'05:40 pm',
-      imgOrden:'zapatillas.png'
-    },
-    {
-      destino:'Col. San Miguel',
-      nombreComprador:'Martha Molina',
-      cantidad:'1 unidad',
-      horaCompra:'05:40 pm',
-      imgOrden:'vestir.png'
-    }
-  ];
+  ordenesDisponibles:any = [];
+  ordenesTomadas:any = [];
+  nombreUsuario: String = '';
+  idUsuario: String = '';
+  ordenProgreso: any = '';
+  ordenesEntregadas: any = [];
+  indiceOrdenProgreso: Number;
 
-  ordenesTomadas:any = [
-    {
-      destino:'Col. Miraflores',
-      nombreComprador:'Camilo Acosta',
-      cantidad:'1 unidad',
-      horaCompra:'04:30 pm',
-      imgOrden:'juguetes.png'
-    },
-    {
-      destino:'Col. Tiloarque',
-      nombreComprador:'Molly Cruz',
-      cantidad:'1 unidad',
-      horaCompra:'04:30 pm',
-      imgOrden:'juguetes.png'
-    },
-    
-  ]
-
-  constructor() { }
+  constructor(
+    private ordenesService: OrdenesService,
+    private _route:Router,
+    private cookiesService: CookieService
+  ) { }
 
   ngOnInit(): void {
+    if (this.cookiesService.check('nanyUsuarioNombre')) {
+      this.nombreUsuario = this.cookiesService.get('nanyUsuarioNombre');
+      this.idUsuario = this.cookiesService.get('nanyUsuarioId');
+
+      this.ordenesService.obtenerOrdenes().subscribe(
+        result=>{
+          let index = -1;
+          for (let i = 0; i < result.length; i++) {
+  
+            if (result[i].tipoEstado.idEstado == "0") {
+              this.ordenesDisponibles.push(result[i]);
+              console.log('Disponible',this.ordenesDisponibles);
+
+            } else if (result[i].tipoEstado.idEstado == "4") {
+              this.ordenesEntregadas.push(result[i]);
+              console.log('Entregadas', this.ordenesEntregadas);
+
+            } else {
+              this.ordenesTomadas.push(result[i]);
+              console.log('Tomada',this.ordenesTomadas);
+              index++;
+              
+              if (this.idUsuario == result[i].motorista._id) {
+                this.ordenProgreso = result[i];
+                this.indiceOrdenProgreso = index;
+                console.log(this.ordenProgreso);
+              } 
+            }  
+          }
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    } 
+  } 
+
+  enOrigen(){
+    this.ordenProgreso.tipoEstado = {
+      idEstado: "2",
+      nombreEstado: "En origen"
+    };
+
+    this.ordenesService.actualizarOrden(this.ordenProgreso._id, this.ordenProgreso).subscribe(
+      result=>{
+        console.log(result);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
   }
 
+  enDestino(){
+    this.ordenProgreso.tipoEstado = {
+      idEstado: "3",
+      nombreEstado: "En destino"
+    };
+
+    this.ordenesService.actualizarOrden(this.ordenProgreso._id, this.ordenProgreso).subscribe(
+      result=>{
+        console.log(result);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+  }
+
+  entregada(){
+    this.ordenProgreso.tipoEstado = {
+      idEstado: "4",
+      nombreEstado: "Entregada"
+    };
+
+    this.ordenesService.actualizarOrden(this.ordenProgreso._id, this.ordenProgreso).subscribe(
+      result=>{
+        console.log(result);
+        Swal.fire({
+          icon: 'success',
+          title: 'Excelente trabajo!',
+          text: 'Has entregado la orden correctamente'
+        });
+        console.log(this.indiceOrdenProgreso);
+        this.ordenesTomadas.splice(this.indiceOrdenProgreso, 1);
+        this.ordenProgreso = '';
+        console.log(this.ordenesTomadas);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+  }
 }
