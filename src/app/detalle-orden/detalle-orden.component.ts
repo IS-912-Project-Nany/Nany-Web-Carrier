d1f1cdae1338074b2131f9bd4bbfa9b85d7ababb
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-08-18 01:28:33 -0600

    [ADD] Services detalle-orden

--- src/app/detalle-orden/detalle-orden.component.ts
+++ src/app/detalle-orden/detalle-orden.component.ts
@@ -1,25 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrdenesService } from '../services/ordenes.service';
+import { CookieService } from 'ngx-cookie-service';
+import { UsuariosService } from '../services/usuarios.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detalle-orden',
   templateUrl: './detalle-orden.component.html'
 })
 export class DetalleOrdenComponent implements OnInit {
-  detalleOrden:any = [
-    {
-      nombreArticulo:'Tenis de colores',
-      descripcion:'Zapato marca Nike, color: Rojo, numero EUR38, USA 8',
-      cantidad:'1 unidad',
-      precio:'Lps. 1,000',
-      categoria:"Zapatos",
-      empresa:'Payless',
-      imgOrden:'zapatillas.png'
-    }
-  ]
+  region: Boolean = true;
+  idOrden: any = '';
+  detalleOrden:any = '';
+  ordenesUsuario: any = [];
+  idUsuario: any  = this.cookiesService.get('nanyUsuarioId');
 
-  constructor() { }
+  constructor(
+    private _route: Router,
+    private ruta: ActivatedRoute,
+    private ordenesService: OrdenesService,
+    private cookiesService: CookieService,
+    private usuariosService: UsuariosService
+  ) { }
 
   ngOnInit(): void {
+    this.ruta.params.subscribe(
+      params => {
+        this.idOrden = params._id;
+      }
+    )
+    
+    this.ordenesService.obtenerOrden(this.idOrden).subscribe(
+      result=>{
+        this.detalleOrden = result;
+        console.log(this.detalleOrden);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+    
+    this.usuariosService.obtenerOrdenesUsuario(this.idUsuario).subscribe(
+      result=>{
+        this.ordenesUsuario = result.ordenes;
+        console.log(this.ordenesUsuario);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+    
   }
 
+  tomarOrden(){
+    let entrega = 0;
+    for (let i = 0; i < this.ordenesUsuario.length; i++) {
+      if (this.ordenesUsuario[i].tipoEstado.idEstado != "4") {
+        entrega++;
+      }
+    }
+
+    if (entrega != 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Tienes ordenes por entregar!',
+        text: 'No puedes tomar mas de una orden a la vez'
+      });
+    } else {
+      this.detalleOrden.tipoEstado = {
+        idEstado: "1",
+        nombreEstado: "En camino"
+      };
+  
+      this.detalleOrden.motorista = {
+        _id: this.cookiesService.get('nanyUsuarioId'),
+        nombre: this.cookiesService.get('nanyUsuarioNombre'),
+        apellido: this.cookiesService.get('nanyUsuarioApellido')
+      };
+  
+      this.ordenesService.actualizarOrden(this.idOrden, this.detalleOrden).subscribe(
+        result=>{
+          console.log(result);
+          this._route.navigate(['/Home']);
+        },
+        error=>{
+          console.log(error);
+        }
+      )
+    }
+  }
 }
