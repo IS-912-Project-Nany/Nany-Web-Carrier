6c2964e166adc9ff29e73e8b3cc899bca7b7e5cc
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-08-19 17:06:38 -0600

    [FIX] Name server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static(__dirname + '/dist/nany-web-carrier'));
+    app.use(express.static(__dirname + '/dist/Nany-Web-Carrier'));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/nany-web-carrier/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Nany-Web-Carrier/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
